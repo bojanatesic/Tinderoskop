29a124bc37fcc4353495e8af8267260425025423
bojanatesic <tesic.bojana@yahoo.com>
2021-07-28 00:42:22 +0200

    second commit

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
+import {IconButton}  from '@material-ui/core';
 import './Header.css'
 function Header() {
     return (
         <div className="header">
-            <PersonIcon />
+            <IconButton className="icon-button">
+            <PersonIcon className="header-icon" fontSize="large" />
+            </IconButton>
             <img className="header-img" src="https://cdn2.iconfinder.com/data/icons/social-media-icons-23/800/tinder-512.png" alt="tinder-logo"/>
-            <ForumIcon/>
+            <IconButton className="icon-button">
+            <ForumIcon className="header-icon" fontSize="large"/>
+            </IconButton>
         </div>
     )
 }
